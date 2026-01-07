2833e56dd08a88f097249b87dbbf1f7cf99bce4e
FacundoJoaquinGil <FacundoJoaGL@gmail.com>
2026-01-07 16:21:46 -0300

    repaso

--- login-practice/home/home.js
+++ login-practice/home/home.js
@@ -1,13 +1,13 @@
-const mensajeBienvenida = document.getElementById("titulo-bienvenida");
+const url = "http://localhost:3000/usuarios";
 const usuarioLogeado = JSON.parse(localStorage.getItem("usuarioLogeado"));
+const mensajeBienvenida = document.getElementById("titulo-bienvenida");
 
+//Experimental
 const fotoPerfil = document.getElementById("fotoPerfil");
-
 fotoPerfil.src = usuarioLogeado.fotoPerfil;
-
+//
 
 mensajeBienvenida.innerHTML = `Bienvenido ${usuarioLogeado.username}`;
-
 parrafoMail.innerHTML = `${usuarioLogeado.mail}`;
 
 function cerrarSesion() {
@@ -15,9 +15,46 @@ function cerrarSesion() {
     window.location.href = "../logeo/logeo.html";
 }
 
+const usuarioId = usuarioLogeado.id;
+
+
+const form = document.getElementById("formTarea");
+const dataTarea = document.getElementById("txtTarea");
+
+form.addEventListener("submit", agregarTarea)
+
+async function agregarTarea(event) {
+    event.preventDefault()
+    try {
+        const usuarioActual = await axios.get(`${url}/${usuarioId}`);
+        const usuarioData = usuarioActual.data;
+        console.log(usuarioData)
+
+        const tarea = dataTarea.value;
+        
+        const data = {
+            id: Date.now(),
+            fecha: fechaCompleta,
+            hora: horaCompelta,
+            tarea: tarea
+        }
+        const nuevasTareas = usuarioData.tareas.push(data);
+        console.log(nuevasTareas)
+
+        await axios.patch(`${url}/${usuarioId}`,{
+            tareas: usuarioData.tareas
+        })
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+//OBTENER FECHA Y HORA
 const fecha = new Date();
 
-//Fecha
+//FECHA
 let año = fecha.getFullYear();
 let mes = fecha.getMonth() + 1;
 let dia = fecha.getDate();
@@ -25,14 +62,17 @@ let dia = fecha.getDate();
 mes = (mes < 10 ? '0' : '') + mes;
 dia = (dia < 10 ? '0' : '') + dia;
 
-console.log(`${dia}/${mes}/${año}`);
+const fechaCompleta = `${dia}/${mes}/${año}`;
 
-//Hora
+
+
+//HORA
 let horas = fecha.getHours();
 let minutos = fecha.getMinutes();
 
 horas = (horas < 10 ? '0' : '') + horas;
 minutos = (minutos < 10 ? '0' : '') + minutos;
 
-console.log(`${horas}:${minutos}`);
+const horaCompelta = `${horas}:${minutos}`
+
 
