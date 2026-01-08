a473f2481c7fa95e6e9ee7756ef9d7a80b60fb09
FacundoJoaquinGil <FacundoJoaGL@gmail.com>
2026-01-08 14:54:32 -0300

    repaso

--- login-practice/home/home.js
+++ login-practice/home/home.js
@@ -1,78 +1,98 @@
 const url = "http://localhost:3000/usuarios";
 const usuarioLogeado = JSON.parse(localStorage.getItem("usuarioLogeado"));
+const usuarioId = usuarioLogeado.id;
 const mensajeBienvenida = document.getElementById("titulo-bienvenida");
+
+mensajeBienvenida.innerHTML = `Bienvenido ${usuarioLogeado.username}`;
+parrafoMail.innerHTML = `${usuarioLogeado.mail}`;
 
 //Experimental
 const fotoPerfil = document.getElementById("fotoPerfil");
 fotoPerfil.src = usuarioLogeado.fotoPerfil;
 //
 
-mensajeBienvenida.innerHTML = `Bienvenido ${usuarioLogeado.username}`;
-parrafoMail.innerHTML = `${usuarioLogeado.mail}`;
+//OBTENER FECHA Y HORA
+const fecha = new Date();
+//FECHA
+let año = fecha.getFullYear();
+let mes = fecha.getMonth() + 1;
+let dia = fecha.getDate();
+mes = (mes < 10 ? '0' : '') + mes;
+dia = (dia < 10 ? '0' : '') + dia;
+const fechaCompleta = `${dia}/${mes}/${año}`;
+//HORA
+let horas = fecha.getHours();
+let minutos = fecha.getMinutes();
+horas = (horas < 10 ? '0' : '') + horas;
+minutos = (minutos < 10 ? '0' : '') + minutos;
+const horaCompelta = `${horas}:${minutos}`
 
 function cerrarSesion() {
     localStorage.removeItem("usuarioLogeado");
     window.location.href = "../logeo/logeo.html";
 }
 
-const usuarioId = usuarioLogeado.id;
-
-
+async function usuarioDataActual() {
+    try {
+        const usuarioActual = await axios.get(`${url}/${usuarioId}`);
+        return usuarioActual.data;
+    } catch (error) {
+        console.log(error);
+    }
+}
 const form = document.getElementById("formTarea");
 const dataTarea = document.getElementById("txtTarea");
-
 form.addEventListener("submit", agregarTarea)
 
 async function agregarTarea(event) {
-    event.preventDefault()
+    event.preventDefault();
     try {
-        const usuarioActual = await axios.get(`${url}/${usuarioId}`);
-        const usuarioData = usuarioActual.data;
-        console.log(usuarioData)
+        const usuarioData = await usuarioDataActual()
 
         const tarea = dataTarea.value;
-        
-        const data = {
+        const nuevaTarea = {
             id: Date.now(),
             fecha: fechaCompleta,
             hora: horaCompelta,
             tarea: tarea
-        }
-        const nuevasTareas = usuarioData.tareas.push(data);
-        console.log(nuevasTareas)
+        };
+        usuarioData.tareas.push(nuevaTarea);
 
-        await axios.patch(`${url}/${usuarioId}`,{
+
+        await axios.patch(`${url}/${usuarioId}`, {
             tareas: usuarioData.tareas
-        })
-
+        });
+        alert("Tarea agregada ✅");
     } catch (error) {
-        console.log(error)
+        console.log(error);
     }
 }
 
 
-//OBTENER FECHA Y HORA
-const fecha = new Date();
 
-//FECHA
-let año = fecha.getFullYear();
-let mes = fecha.getMonth() + 1;
-let dia = fecha.getDate();
+async function dibujarTareas() {
+    const contenedorTareas = document.getElementById("contenedorTareas");
 
-mes = (mes < 10 ? '0' : '') + mes;
-dia = (dia < 10 ? '0' : '') + dia;
+    const pFecha = document.getElementById("pFecha");
+    const pHora = document.getElementById("pHora");
+    const pTarea = document.getElementById("pTarea");
 
-const fechaCompleta = `${dia}/${mes}/${año}`;
+    const data = await usuarioDataActual();
+    const dataTareas = data.tareas
 
+    dataTareas.forEach(t => {
+        contenedorTareas.innerHTML += `
+            <div class="encabezado-tarea">
+                <input type="checkbox" class="input-checkbox">
+                <p id="pFecha" class="pFecha">${t.fecha}</p>
+                <p id="pHora" class="pHora">${t.hora}</p>
+                <button class="btn-eliminar">Eliminar</button>
+            </div>
 
+            <div class="contenedor-comentario-tarea">
+                <p id="pTarea" class="pTarea">${t.tarea}</p>
+            </div> `
+    });
+}
 
-//HORA
-let horas = fecha.getHours();
-let minutos = fecha.getMinutes();
-
-horas = (horas < 10 ? '0' : '') + horas;
-minutos = (minutos < 10 ? '0' : '') + minutos;
-
-const horaCompelta = `${horas}:${minutos}`
-
-
+dibujarTareas()
